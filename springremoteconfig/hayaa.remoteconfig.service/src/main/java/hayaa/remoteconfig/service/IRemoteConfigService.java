c1138ebc92639ec791099d6ef167615b83ce9814
hayaa <dev@hayaa-ai.com>
2018-12-10 16:03:09 +0800

    修复rpc的函数参数类型造成的错误。由于int等类型不是类，在rpc代理反射时会产生类型不匹配造成的无法找到构造函数的问题。

--- springremoteconfig/hayaa.remoteconfig.service/src/main/java/hayaa/remoteconfig/service/IRemoteConfigService.java
+++ springremoteconfig/hayaa.remoteconfig.service/src/main/java/hayaa/remoteconfig/service/IRemoteConfigService.java
@@ -3,6 +3,6 @@ package hayaa.remoteconfig.service;
 import hayaa.basemodel.model.FunctionResult;
 
 public interface IRemoteConfigService {
-    FunctionResult<AppConfig> SendConfig(String solutionID, int version);
-    FunctionResult<AppConfig> SendJsAppConfig(String solutionID, int version);
+    FunctionResult<AppConfig> SendConfig(String solutionID, Integer version);
+    FunctionResult<AppConfig> SendJsAppConfig(String solutionID, Integer version);
 }
